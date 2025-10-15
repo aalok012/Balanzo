e243be72d20a09b0305cc8a174bd97aca15dcc35
Alok <boyeeaalok@gmail.com>
2025-10-15 18:53:32 -0500

    utils setup basic

---
+++ backend/utils/ApiResponse.js
@@ -0,0 +1,12 @@
+class ApiResponse {
+    constructor(statusCode,
+        message,
+        data,
+        success
+    ){
+        this.message="success"
+        this.statusCode=statusCode
+        this.data= data
+        
+    }
+}
--- backend/utils/asyncHandler.js
+++ backend/utils/asyncHandler.js
@@ -1,7 +1,24 @@
-asyncHandler(func)=>async()=>{
-Promise.resolve(
+// const asyncHandler = (requestHandler) => {
+// (req,res,next)=> { Promise.resolve(requestHandler(req,res,next)
+// ).reject( (error)=> {
+//     status: 404,
+//     message: "error.message"
+//  }
+// )
+// }
+// }
 
-).reject(
- 
-)
+
+//wrapper function for the handling async function everywhere
+
+const asyncHandler = (fn) =>async(req,res,next) => {
+    try{
+           await fn(req,res,next)
+    }
+    catch(error){
+            res.status(error.code || 404).json({
+                success: false,
+                message: error.message
+            })
+    }
 }
