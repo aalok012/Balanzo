c66c663a5dc1c7e297dc4cd2d8c8a63f6db458ed
Alok <boyeeaalok@gmail.com>
2025-12-28 03:00:32 -0600

    Update API base URL config

--- frontend/src/axiosApi.jsx
+++ frontend/src/axiosApi.jsx
@@ -1,14 +1,17 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api/v1",
+  baseURL: `${import.meta.env.VITE_API_URL}/api/v1`, //
+  withCredentials: true, // keep if you use auth/cookies
 });
+
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
 
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
