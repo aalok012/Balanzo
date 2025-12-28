845f499f8b1ce402f0736af152c96cd58454df10
Alok <boyeeaalok@gmail.com>
2025-12-28 03:02:53 -0600

    Adjust backend route prefixes

--- backend/app.js
+++ backend/app.js
@@ -17,8 +17,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // routes declaration
-app.use("/api/v1/users", userRouter); // if the user types /users the control goes to the userRouter.
-app.use("/api/v1/expenses" ,expenseRouter) // if the user types/ expense the control goes to the expenseRouter
+app.use("/users", userRouter); // if the user types /users the control goes to the userRouter.
+app.use("/expenses" ,expenseRouter) // if the user types/ expense the control goes to the expenseRouter
 
 //Example of the direction of route
 // http://localhost:8000/api/v1/users/register
